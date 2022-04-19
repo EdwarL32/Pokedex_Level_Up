2714e4c4116d6bb6262635023b8d416227b7ae2f
Edwar Esaul López Pérez <edwarelp@gmail.com>
2022-04-18 18:30:15 -0600

    Finalizado con Login

--- pokedex/src/components/PokemonsCards.js
+++ pokedex/src/components/PokemonsCards.js
@@ -13,7 +13,10 @@ const PokemonsCards = ({ pokemonsUrl }) => {
 
     }, [pokemonsUrl])
 
+
+
     return (
+        
         <li className='col'>
             <Link to={`/pokemons/${pokemons.id}`} className="pokemons-card" >
                 <h3>{pokemons.name}</h3>
@@ -21,7 +24,7 @@ const PokemonsCards = ({ pokemonsUrl }) => {
 
                 <ul className='location__list'>
                     <li cl><span className='lotation_list-subtitle'> </span><b>Tipo </b>{pokemons.types?.[0].type.name}</li>
-                    <li cl><span className='lotation_list-subtitle' > </span><b>altura</b> {pokemons.height}M</li>
+                    <li cl><span className='lotation_list-subtitle' > </span><b>Altura</b> {pokemons.height}M</li>
                     <li cl><span className='lotation_list-subtitle'> </span><b>Peso</b> {pokemons.weight}Kg</li>
                 </ul>
 
